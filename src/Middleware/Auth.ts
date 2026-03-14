1c50b846bf0b91b7b118f0745dd0500ad013a753
Zzelen <cristian.lop.gonzalez@gmail.com>
2026-03-14 12:40:19 +0100

    testing

--- src/Middleware/Auth.ts
+++ src/Middleware/Auth.ts
@@ -1,10 +1,6 @@
 ﻿import {Request, Response, NextFunction} from 'express';
 
 export const requireApiKey = (req: Request, res: Response, next: NextFunction): void => {
-
-    console.log('API_SECRET_KEY:', process.env.API_SECRET_KEY);
-    console.log('Header recibido:', req.headers['x-api-key']);
-
     const apiKey = req.headers['x-api-key'];
 
     if (!apiKey || apiKey !== process.env.API_SECRET_KEY) {

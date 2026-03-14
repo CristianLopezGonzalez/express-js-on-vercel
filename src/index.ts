632d5198af455232577ee906e45716effeca106b
Zzelen <cristian.lop.gonzalez@gmail.com>
2026-03-14 18:32:09 +0100

    fix url

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,10 @@ const __dirname = path.dirname(__filename)
 const app = express()
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://vite-react-ecru-six-56.vercel.app','https://vite-react-fu1arwcvy-cristianlopgonzalez-7331s-projects.vercel.app']
+    origin: [
+        'http://localhost:5173',
+        /\.vercel\.app$/
+    ]
 }))
 
 app.use(express.json())

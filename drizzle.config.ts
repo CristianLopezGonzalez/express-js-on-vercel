f421db1878d02f3657b54a168e7e759135427d0f
Zzelen <cristian.lop.gonzalez@gmail.com>
2026-03-17 16:39:46 +0100

    fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
 
 export default defineConfig({
-    schema: "./src/db/schema.ts",
+    schema: "./src/db/schemas/*.ts",
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
